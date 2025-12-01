1aa0a3ab609b274019ebad0a4a2dd9b7a720b4a7
Eyal Levin <eyal@vetric.io>
2025-12-01 20:12:34 +0200

    fix(examples): correct provider path and example files (#1)

--- examples/resources/scylladbcloud_vpc_peering/aws.tf
+++ examples/resources/scylladbcloud_vpc_peering/aws.tf
@@ -1,4 +1,16 @@
 # End-to-end example for ScyllaDB Datacenter VPC peering on AWS.
+resource "scylladbcloud_cluster" "example" {
+	name       = "My Cluster"
+	cloud      = "AWS"
+	region     = "us-east-1"
+	node_count = 3
+	node_type  = "i3.xlarge"
+	cidr_block = "172.31.0.0/16"
+
+	enable_vpc_peering = true
+	enable_dns         = true
+}
+
 resource "aws_vpc" "app" {
 	cidr_block = "10.0.0.0/16"
 }
@@ -6,13 +18,13 @@ resource "aws_vpc" "app" {
 data "aws_caller_identity" "current" {}
 
 resource "scylladbcloud_vpc_peering" "example" {
-	cluster_id = 1337
-	datacenter = "AWS_EAST_1"
+	cluster_id = scylladbcloud_cluster.example.cluster_id
+	datacenter = scylladbcloud_cluster.example.datacenter
 
-	peer_vpc_id     = aws_vpc.app.id
-	peer_cidr_block = aws_vpc.app.cidr_block
-	peer_region     = "us-east-1"
-	peer_account_id = data.aws_caller_identity.current.account_id
+	peer_vpc_id       = aws_vpc.app.id
+	peer_cidr_blocks  = [aws_vpc.app.cidr_block]
+	peer_region       = "us-east-1"
+	peer_account_id   = data.aws_caller_identity.current.account_id
 
 	allow_cql = true
 }
@@ -26,7 +38,7 @@ resource "aws_route_table" "app" {
 	vpc_id = aws_vpc.app.id
 
 	route {
-		cidr_block = scylladbcloud_cluster.example.cidr_block
+		cidr_block                = scylladbcloud_cluster.example.cidr_block
 		vpc_peering_connection_id = aws_vpc_peering_connection_accepter.app.vpc_peering_connection_id
 	}
 }
--- examples/resources/scylladbcloud_vpc_peering/resource.tf
+++ examples/resources/scylladbcloud_vpc_peering/resource.tf
@@ -3,10 +3,10 @@ resource "scylladbcloud_vpc_peering" "example" {
 	cluster_id = 1337
 	datacenter = "AWS_US_EAST_1"
 
-	peer_vpc_id     = "vpc-1234"
-	peer_cidr_block = "192.168.0.0/16"
-	peer_region     = "us-east-1"
-	peer_account_id = "123"
+	peer_vpc_id      = "vpc-1234"
+	peer_cidr_blocks = ["192.168.0.0/16"]
+	peer_region      = "us-east-1"
+	peer_account_id  = "123"
 
 	allow_cql = true
 }
